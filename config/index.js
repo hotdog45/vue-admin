74ad85db668926e263b6c9cfe18c815cc5f0cb24
李顺风 <mac2020@126.com>
2018-12-07 10:57:46 +0800

    正式

--- config/index.js
+++ config/index.js
@@ -29,8 +29,9 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/apis': {
-        target: 'http://120.24.212.29:3000',
+        // target: 'http://120.24.212.29:3000',
         //设置你调用的接口域名和端口号 别忘了加http
+             target: "https://api.jamootime.com", // 接口域名
         changeOrigin: true,
         pathRewrite: {
           '^/apis': ''//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
